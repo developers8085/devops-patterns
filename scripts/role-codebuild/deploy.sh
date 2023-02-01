ad36e2eff05e90331adfe2b5bac817f0af56992e
suresh <jadav.suresh@capgemini.com>
2023-02-01 19:28:50 +0530

    latest updates

--- scripts/role-codebuild/deploy.sh
+++ scripts/role-codebuild/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo yum install jq
 function stack_creation(){
     STACK_NAME=$1
     if [ -z "$1" ]
@@ -7,24 +8,26 @@ function stack_creation(){
         echo "No STACK_NAME argument supplied"
         exit 1
     fi
-    echo $DIR
-    echo "Creating stack..."
+
+    echo "Creating Stack..."
     echo ${STACK_NAME}
-    # echo ${DIR}
+    echo ${DIR}
+    echo file://${DIR}/tags.json
+    echo file://${DIR}/$2
+    echo file://$DIR/parameters.json
     STACK_ID=$( \
     aws cloudformation create-stack \
     --stack-name $1 \
-    --template-body file://$DIR/$2 \
+    --template-body file://${DIR}/$2 \
     --capabilities CAPABILITY_NAMED_IAM \
-    # --parameters file://$DIR/parameters.json \
-    --tags file://$DIR/tags.json \
+    --parameters file://$DIR/parameters.json \
+    --tags file://${DIR}/tags.json \
     --region $3
     # | jq -r .StackId \
     )
-    echo "Waiting on ${STACK_ID} create completion..."
-    aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
-    aws cloudformation describe-stacks --stack-name ${STACK_ID} 
-    # | jq .Stacks[0].Outputs
+    echo "Waiting on ${STACK_NAME} create completion..."
+    aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME} --region $3
+    # aws cloudformation describe-stacks --stack-name ${STACK_NAME} --region $3
 }
 
 stack_creation ec2InstanceProfile ec2InstanceProfile.yaml us-east-1
