c602f9e505e8afaef8bd3695b16227346522028f
suresh <jadav.suresh@capgemini.com>
2023-02-01 18:07:10 +0530

    scripts added to execute cloudformation stack

--- scripts/role-codebuild/deploy.sh
+++ scripts/role-codebuild/deploy.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
-# STACK_NAME=$1
-# if [ -z "$1" ]
-#   then
-#     echo "No STACK_NAME argument supplied"
-#     exit 1
-# fi
-# DIR="
 
-# echo "Creating stack..."
-# STACK_ID=$( \
-#   aws cloudformation create-stack \
-#   --stack-name ${STACK_NAME} \
-#   --template-body file://${DIR}/cron-batch-stack.yml \
-#   --capabilities CAPABILITY_IAM \
-#   --parameters file://${DIR}/parameters.json \
-#   --tags file://${DIR}/tags.json \
-#   | jq -r .StackId \
-# )
+function stack_creation(){
+    STACK_NAME=$1
+    if [ -z "$1" ]
+    then
+        echo "No STACK_NAME argument supplied"
+        exit 1
+    fi
+    echo {$DIR}
+    echo "Creating stack..."
+    STACK_ID=$( \
+    aws cloudformation create-stack \
+    --stack-name ${STACK_NAME} \
+    --template-body file://${DIR}/$2 \
+    --capabilities CAPABILITY_IAM \
+    --parameters file://${DIR}/parameters.json \
+    --tags file://${DIR}/tags.json \
+    --region $3
+    # | jq -r .StackId \
+    )
+    echo "Waiting on ${STACK_ID} create completion..."
+    aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
+    aws cloudformation describe-stacks --stack-name ${STACK_ID} 
+    # | jq .Stacks[0].Outputs
+}
 
-# echo "Waiting on ${STACK_ID} create completion..."
-# aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
-# aws cloudformation describe-stacks --stack-name ${STACK_ID} | jq .Stacks[0].Outputs
-
-echo "Suresh Jadav"
-echo "Suresh Jadav"
-echo "Suresh Jadav"
-echo "Suresh Jadav"
-echo "Suresh Jadav"
-echo "Suresh Jadav"
+stack_creation ec2InstanceProfile ec2InstanceProfile.yaml us-east-1
